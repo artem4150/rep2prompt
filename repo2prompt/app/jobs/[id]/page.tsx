8d6e5af989323fa817cea314b8527af9c620e19a
artem4150 <52111050+artem4150@users.noreply.github.com>
2025-09-22 04:20:24 +0500

    Delete repo2prompt directory

--- repo2prompt/app/jobs/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import JobProgress from '@/components/JobProgress';
-
-type JobPageParams = Promise<{ id: string }> | { id: string };
-
-export default async function JobPage({ params }: { params: JobPageParams }) {
-  const resolved = params instanceof Promise ? await params : params;
-  return <JobProgress jobId={resolved.id} />;
-}
--- repo2prompt/app/result/[exportId]/page.tsx
+++
@@ -1,8 +0,0 @@
-import ArtifactsList from '@/components/ArtifactsList';
-
-type ResultPageParams = Promise<{ exportId: string }> | { exportId: string };
-
-export default async function ResultPage({ params }: { params: ResultPageParams }) {
-  const resolved = params instanceof Promise ? await params : params;
-  return <ArtifactsList exportId={resolved.exportId} />;
-}
